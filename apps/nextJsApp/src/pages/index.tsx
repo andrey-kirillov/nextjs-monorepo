fe4207a0dfd7d16f8e0eb58a6f63cf0d902395cc
andrey_kirillov <andrey.kirillov.kiev@gmail.com>
2025-04-13 17:34:59 +0300

    test cache

--- apps/nextJsApp/src/pages/index.tsx
+++ apps/nextJsApp/src/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome @ak/nextJsApp 👋
+              Welcome @ak/nextJsApp TEST Cache👋
             </h1>
           </div>
 
