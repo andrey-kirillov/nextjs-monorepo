33169f0b406535b8e3ea852ec8346e9d770cdc02
andrey_kirillov <andrey.kirillov.kiev@gmail.com>
2025-04-13 18:17:04 +0300

    test CI

--- apps/nextJsApp/src/pages/index.tsx
+++ apps/nextJsApp/src/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome @ak/nextJsApp TEST Deploy👋
+              Welcome @ak/nextJsApp TEST CI👋
             </h1>
           </div>
 
